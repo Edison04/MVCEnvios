137c5503f450b657187e7f0ca5e9b9915a1fd443
Karloz <karloz.jaramillo@hotmail.com>
2020-12-16 20:17:59 -0500

    Se agrega funcionalidad de filtrar y se aplica a Clientes

--- MVCEnvios/Scripts/General.js
+++ MVCEnvios/Scripts/General.js
@@ -3,4 +3,11 @@
     $('.datepicker').datepicker();
     $('select').formSelect();
     $('.slider').slider();
+
+    $("#Filtrar").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#filtro tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
 });
